43385f42eefe55cfff95fced10c11015feb1949f
Kira Gbedan <161087999+Verbalase@users.noreply.github.com>
2025-04-15 20:38:09 -0500

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -16,7 +16,7 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://cdn.remstations.com"
-FORK_ID = "RemStationLRP"
+FORK_ID = "RemStationFork"
 
 def main():
     parser = argparse.ArgumentParser()
