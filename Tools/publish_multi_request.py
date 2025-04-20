23edff8198af0109a548622c89ad15ee8a0bec8d
Kira Gbedan <161087999+Verbalase@users.noreply.github.com>
2025-04-19 20:01:58 -0500

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.remstations.com"
+ROBUST_CDN_URL = "https://cdn.remstations.com/"
 FORK_ID = "MyCoolFork"
 
 def main():
